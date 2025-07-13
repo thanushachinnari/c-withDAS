b60e93200153948185830a31e7fe430b91bc2960
thanushachinnari <thanushachinnari2003@gmail.com>
2025-07-13 16:27:43 +0530

    array problems

--- jul12/zeroesatend.cpp
+++ jul12/zeroesatend.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 using namespace std;
 #include <vector>
-int zeroatend(vector<int> arr){
-  int result ;
-  int n = 
+void zeroatend(vector<int>& arr){
+  // int result ;
+  int n = arr.size();
+  int j =0;
+  for(int i =0;i<n;i++){
+    if(arr[i] !=0){
+     swap(arr[i], arr[j]);
+     j++;
+    }
+  }
+
 }
-int main(){
-  vector<int> arr={1,0,4,0,56,9};
-  return 0;
+int main() {
+    vector<int> arr = {1, 0, 4, 0, 56, 9};
+
+    zeroatend(arr);
+
+
+    for(int num : arr) {
+        cout << num << " ";
+    }
+
+    return 0;
 }
