3ca32facb4a766bda8ce6896d9c3c09209b6e27c
thanushachinnari <thanushachinnari2003@gmail.com>
2025-07-19 18:20:34 +0530

    MaximumConsecutiveOnes

--- jul 19/MaximumConsecutiveOnes.cpp
+++ jul 19/MaximumConsecutiveOnes.cpp
@@ -3,22 +3,24 @@ using namespace std;
 #include<vector>
 
 int MaximumConsecutiveOnes(vector<int> arr){
-  int n = arr.size();
-  int max_count =0;
-  int current_count =0;
-  for(int i = 0;i<n;i++){
+  int n = arr.size(); //finding size of the array
+  int max_count =0;  // count the maxnumber
+  int current_count =0;    // count the current value
+  for(int i = 0;i<n;i++){    
        if(arr[i] ==1){
-        current_count++;
-        max_count = max(max_count,current_count);
+        current_count++;   //if arr[i] equal to 1 then current value is increaments
+        max_count = max(max_count,current_count);   //  it finds the hightest value
+        //  form the maxvalue and current value
+
        }
        else{
-        current_count =0;
+        current_count =0; //if arr[i]==0 then current value is reset
     
        }
   }
 
 
-     return max_count;
+     return max_count;   // returns max_values of 1's
 }
 int main()
 {
