b4e8ee3a95939e4b2e102de8a94853abd54c9b5f
thanushachinnari <thanushachinnari2003@gmail.com>
2025-07-22 15:44:09 +0530

    pattern

--- jul 22/pattern_9.cpp
+++ jul 22/pattern_9.cpp
@@ -6,3 +6,46 @@
  ***
   *
 */
+#include<iostream>
+using namespace std;
+void patterns(int n){
+
+
+     for(int i =0;i<=n;i++){
+        // space
+        for(int j =0;j<n-i;j++){
+            cout<<" ";
+        }
+        // stars
+         for(int j =0;j<2*i+1;j++){
+          cout<<"*";
+        }
+        // space
+        //  for(int j =0;j<n-i+1;j++){
+        //     cout<<" ";
+        // }
+        cout<<endl;
+      }
+
+  
+      for(int i =0;i<=n;i++){
+        // space
+        for(int j =0;j<=i;j++){
+            cout<<" ";
+        }
+        // stars
+         for(int j =0;j<=2*(n-i)-2;j++){
+          cout<<"*";
+        }
+        // space
+        //  for(int j =0;j<=i;j++){
+        //     cout<<" ";
+        // }
+        cout<<endl;
+      }
+
+}
+int main(){
+  patterns(3);
+  return 0;
+}
